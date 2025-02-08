0e07e17adebb7a83ed2f440a0ad63b53d1495af3
exxardo <eduardoramirodematos@gmail.com>
2025-02-08 18:55:26 -0300

    Initial commit

---
+++ src/main/java/com/desafio/casepixitau/CasePixItauApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.casepixitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasePixItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasePixItauApplication.class, args);
+	}
+
+}
