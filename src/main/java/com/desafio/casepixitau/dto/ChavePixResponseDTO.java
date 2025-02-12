05456ba1054f20a154211e2ff96c7198e99794f4
exxardo <eduardoramirodematos@gmail.com>
2025-02-12 06:37:54 -0300

    Revert "feat: adicionar campo dataHoraInativacao na resposta e impedir consultas/alterações de chaves inativas"
    This reverts commit 2eb2471f7d048adc93df22c9a7b5d542f0024701.

--- src/main/java/com/desafio/casepixitau/dto/ChavePixResponseDTO.java
+++ src/main/java/com/desafio/casepixitau/dto/ChavePixResponseDTO.java
@@ -20,5 +20,4 @@ public class ChavePixResponseDTO {
     private String nomeCorrentista;
     private String sobrenomeCorrentista;
     private LocalDateTime dataHoraInclusao;
-    private LocalDateTime dataHoraInativacao;
 }
